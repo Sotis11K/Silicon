a9ebfc804cc32dd029a584e8ba40b55f31c588fb
Sotis11K <brian20041211@gmail.com>
2024-03-08 16:34:43 +0100

    bug fixes on title

---
+++ WebApp.RazorPages/Pages/SignUp.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.RazorPages.Pages
+{
+    public class SignUpModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
