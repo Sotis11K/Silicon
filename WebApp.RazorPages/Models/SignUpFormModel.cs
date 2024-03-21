7f1bec9cdb0818f35baf7142d76f8a271d44367f
Sotis11K <brian20041211@gmail.com>
2024-03-21 12:00:41 +0100

    lunch break

---
+++ WebApp.RazorPages/Models/SignUpFormModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
+
+namespace WebApp.RazorPages.Models;
+
+public class SignUpFormModel
+{
+    [DisplayName ("First Name")]
+    [Required(ErrorMessage = "Invalid last name")]
+    public string FirstName { get; set; } = null;
+
+    [DisplayName("Last Name")]
+    [Required(ErrorMessage = "Invalid last name")]
+    public string LastName { get; set; } = null;
+
+    [DisplayName("Email address")]
+    [Required(ErrorMessage = "Invalid email address")]
+    public string Email { get; set; } = null;
+
+    [DisplayName("Password")]
+    [Required(ErrorMessage = "Invalid password")]
+    public string Password { get; set; } = null;
+
+    [DisplayName("Confirm password")]
+    [Required(ErrorMessage = "Must confirm password")]
+    [Compare(nameof(Password), ErrorMessage = "Passwords do not match")]
+    public string ConfirmPassword { get; set; } = null;
+
+
+    [DisplayName("Term & Conditions")]
+    [Required(ErrorMessage = "You must agree to the terms & conditions")]
+    public bool Terms { get; set; }
+}
--- WebApp.RazorPages/Pages/SignUp.cshtml.cs
+++ WebApp.RazorPages/Pages/SignUp.cshtml.cs
@@ -1,12 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApp.RazorPages.Models;
 
-namespace WebApp.RazorPages.Pages
+namespace WebApp.RazorPages.Pages;
+
+public class SignUpModel : PageModel
 {
-    public class SignUpModel : PageModel
+    public SignUpFormModel Form { get; set; } = null!;
+
+
+
+    public void OnGet()
     {
-        public void OnGet()
-        {
-        }
+        Form = new SignUpFormModel();
+    }
+
+    public void OnPost()
+    {
     }
 }
