68b611c46386a7a022b1e4afbf06b69d5572b48e
Sotis11K <brian20041211@gmail.com>
2024-04-08 01:49:07 +0200

    finished setting up account details and styling it. it is done in razor pages and needs to be fixed to handle 2 forms

---
+++ WebApp.RazorPages/Models/AccountDetailsAddressInfoModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.RazorPages.Models
+{
+    public class AccountDetailsAddressInfoModel
+    {
+
+        [Display(Name = "Address", Prompt = "Enter your address", Order = 0)]
+        [Required(ErrorMessage = "Invalid first name")]
+        public string Addressline_1 { get; set; } = null;
+
+        [Display(Name = "Address 2", Prompt = "Enter your address", Order = 1)]
+
+        public string? Addressline_2 { get; set; }
+
+
+        [Display(Name = "Postal code", Prompt = "Enter your postal code", Order = 2)]
+        [Required(ErrorMessage = "Postal code is required")]
+        [DataType(DataType.PostalCode)]
+
+        public string PostalCode { get; set; } = null;
+
+        [Display(Name = "City", Prompt = "Enter your City", Order = 3)]
+        [Required(ErrorMessage = "City is required ")]
+
+
+        public string City { get; set; } = null;
+    }
+}
---
+++ WebApp.RazorPages/Pages/Account/Details.cshtml.cs
@@ -0,0 +1,29 @@
+// DetailsModel.cshtml.cs
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApp.RazorPages.Models; // Make sure to include the namespace of your view model
+
+namespace WebApp.RazorPages.Pages.Account
+{
+    public class DetailsModel : PageModel
+    {
+        public string Title { get; set; } = "Account Details";
+
+        public AccountDetailsBasicInfoModel BasicInfo { get; set; } = new AccountDetailsBasicInfoModel()
+        {
+            ProfileImage = "/images/profile_picture.svg",
+            FirstName = "Ella",
+            LastName = "Kihlgren",
+            Email = "john.doe@domain.com"
+        };
+
+        public AccountDetailsAddressInfoModel AddressInfo { get; set; } = new AccountDetailsAddressInfoModel();
+
+        public IActionResult OnPost()
+        {
+            return RedirectToPage("/account/details");
+        }
+
+    }
+}
