a5d85d44c9a0ab0e8bcc9e96c32e75d19d0b3169
Sotis11K <brian20041211@gmail.com>
2024-04-08 19:57:21 +0200

    messed up, in the wrong branch

--- Infrastructure/Contexts/DataContext.cs
+++ Infrastructure/Contexts/DataContext.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Contexts
 {
     public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
     {
+        public DbSet<AddressEntity> Address { get; set; }
+        public DbSet<UserEntity> User { get; set; }
     }
 }
